a9e255d4e03b09a643de4e4f04f7edde38a92bf1
Devansh Maurya <Devansh233@gmail.com>
2019-02-18 07:43:23 +0530

    [COMPILER DESIGN LAB 3] Added code for symbol table

---
+++ Compiler Design/LAB 3/main.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+bool isOperator(char);
+bool isAlphabet(char);
+bool isDigit(char);
+bool isIdentifier(const char *, int, int);
+void printSymbolTableEntry(const char*, int, int, const char*);
+
+int main() {
+    char str[100];
+    int i = 0, j = 0;
+
+    printf("Enter an string: ");
+    scanf("%s", str);
+
+    printf("You entered: %s\n\n", str);
+
+    printf("Symbol\t\t\tAddress\t\t\t\t\tType\n\n");
+
+    for (i = 0; str[i] != NULL; ++i) {
+
+        if (isOperator(str[i])) {
+            printSymbolTableEntry(str, i, j, "operator");
+            continue;
+        } else {
+            for (j = i; !isOperator(str[j]) && str[j] != NULL && str[j] != ' '; ++j);
+            if(isIdentifier(str, i, j-1))
+                printSymbolTableEntry(str, i, j-1, "identifier");
+            i=j;
+        }
+    }
+
+    return 0;
+}
+
+bool isOperator(char c) {
+    return (c == '+') || (c == '-') || (c == '*') || (c == '/') || (c == '=');
+}
+
+void printSymbolTableEntry(const char *ptr, int start, int end, const char *type) {
+    for (int i = start; i <= end+1; ++i) {
+        printf("%c", ptr[i]);
+    }
+    printf("\t\t\t\t%p\t\t\t%s\n", ptr + start, type);
+}
+
+bool isIdentifier(const char* token, int start, int end) {
+
+    if (isAlphabet(token[0]) || token[0] == '_') {
+        for (int i = start; i <= end ; ++i) {
+            if (!isAlphabet(token[i]) && token[i] != '_' && !isDigit(token[i]))
+                return false;
+        }
+        return true;
+    }
+    else
+        return false;
+}
+
+bool isAlphabet(char c) {
+    if(tolower(c) >= 'a' && tolower(c) <= 'z')
+        return true;
+    else
+        return false;
+}
+
+bool isDigit(char c) {
+    if (c >= '0' && c <= '9')
+        return true;
+    else
+        return false;
+}
