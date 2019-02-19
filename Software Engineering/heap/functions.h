e53e30ebfdbde625fc3d04497c850928389161ed
Devansh Maurya <Devansh233@gmail.com>
2019-02-19 08:02:52 +0530

    Added UML use case diagram for library management software

--- Software Engineering/heap/functions.h
+++
@@ -1,5 +0,0 @@
-
-void insert(int* array, int num, int location);
-void deleteElement(int* array, int num, int size);
-void display(const int* array, int size);
-void create(int* array, int arraySize, int maxSize);
