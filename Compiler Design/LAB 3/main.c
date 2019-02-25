6b98b3eea1905715e72ad2d8697956b26d33b2ed
Devansh-Maurya <devansh233@gmail.com>
2019-02-25 21:38:25 +0530

    Added codes for compiler design labs

--- Compiler Design/LAB 3/main.c
+++ Compiler Design/LAB 3/main.c
@@ -1,17 +1,26 @@
+//TODO: Implement interfaces for deletion and searching
+
 #include <stdio.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
 
 bool isOperator(char);
 bool isAlphabet(char);
 bool isDigit(char);
 bool isIdentifier(const char *, int, int);
 void printSymbolTableEntry(const char*, int, int, const char*);
+int addToSymbolTable(const char*, int, int, const char*);
+void printSymbolTable();
 
-//struct SymbolTable {
-//    char symbol[20];
-//    long
-//};
+struct SymbolTable {
+    char symbol[20];
+    void *address;
+    char type[20];
+} symbolTable[20];
+
+int symbolTableIndex = -1;
 
 int main() {
     char str[100];
@@ -22,8 +31,6 @@ int main() {
 
     printf("You entered: %s\n\n", str);
 
-    printf("Symbol\t\t\tAddress\t\t\t\t\tType\n\n");
-
     for (i = 0; str[i] != NULL; ++i) {
 
         while (str[i] == ' ') {
@@ -32,11 +39,14 @@ int main() {
 
         if (!isOperator(str[i])) {
             for (j = i; !isOperator(str[j]) && str[j] != NULL && str[j] != ' '; ++j);
-            if(isIdentifier(str, i, j-1))
-                printSymbolTableEntry(str, i, j-1, "identifier");
+            if(isIdentifier(str, i, j-1)) {
+                addToSymbolTable(str, i, j-1, "identifier");
+            }
             i=j-1;
         }
     }
+
+    printSymbolTable();
 
     return 0;
 }
@@ -49,7 +59,46 @@ void printSymbolTableEntry(const char *ptr, int start, int end, const char *type
     for (int i = start; i <= end; ++i) {
         printf("%c", ptr[i]);
     }
-    printf("\t\t\t\t%p\t\t\t%s\n", ptr + start, type);
+    printf("\t\t\t\t%p\t\t\t%s\n", (void *) (ptr + start), type);
+}
+
+int addToSymbolTable(const char *ptr, int start, int end, const char *type) {
+
+    int i;
+    char *tempStr = malloc(20*sizeof(char));
+
+    for (int j = start; j <= end ; ++j) {
+        tempStr[j-start] = ptr[j];
+    }
+    tempStr[end - start + 1] = '\0';
+
+    for (i = 0; i <= symbolTableIndex; ++i) {
+        if (!strcmp(symbolTable[i].symbol, tempStr)) {
+            free(tempStr);
+            return i;
+        }
+    }
+
+    symbolTableIndex++;
+    strcpy(symbolTable[symbolTableIndex].symbol, tempStr);
+    symbolTable[symbolTableIndex].address = (void *) ptr + start;
+    strcpy(symbolTable[symbolTableIndex].type, type);
+
+    free(tempStr);
+
+    return symbolTableIndex;
+}
+
+void printSymbolTable() {
+    printf("Symbol\t\t\tAddress\t\t\t\t\tType\n\n");
+
+    struct SymbolTable currentSymbol;
+
+    for (int i = 0; i <= symbolTableIndex; ++i) {
+        currentSymbol = symbolTable[i];
+        printf("%s\t\t\t\t%p\t\t\t%s\n", currentSymbol.symbol,
+                currentSymbol.address, currentSymbol.type);
+    }
 }
 
 bool isIdentifier(const char* token, int start, int end) {
